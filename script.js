4faf2fcfa17b6372bc6484e4e7dec43e1a8cab2c
muhammadgasanov10 <muhammadaligasanov8@gmail.com>
2023-07-17 01:01:58 -0400

    added script and style

---
+++ script.js
@@ -0,0 +1,58 @@
+const result = document.getElementById("result");
+let currentOperator = "";
+let firstNumber = "";
+let secondNumber = "";
+
+function setNumber(number) {
+  if (currentOperator && secondNumber) {
+    result.innerHTML += number;
+    secondNumber += number;
+  } else if (!currentOperator) {
+    if (result.innerHTML === "0") {
+      firstNumber = number;
+      result.innerHTML = number;
+    } else {
+      firstNumber += number;
+      result.innerHTML += number;
+    }
+  } else {
+    secondNumber = number;
+    result.innerHTML = number;
+  }
+}
+
+function setOperator(operator) {
+  currentOperator = operator;
+}
+
+function resulting() {
+  switch (currentOperator) {
+    case "+":
+      result.innerHTML = +firstNumber + +secondNumber;
+      break;
+    case "-":
+      result.innerHTML = +firstNumber - +secondNumber;
+      break;
+    case "*":
+      result.innerHTML = +firstNumber * +secondNumber;
+      break;
+    case "/":
+      result.innerHTML = +firstNumber / +secondNumber;
+      break;
+    case "%":
+      result.innerHTML = (+firstNumber * +secondNumber) / 100;
+      break;
+  }
+}
+
+function clean() {
+  result.innerHTML = 0;
+  currentOperator = "";
+  firstNumber = "";
+  secondNumber = "";
+}
+
+const time = document.getElementById("time");
+setInterval(() => {
+  time.innerHTML = new Date().toLocaleTimeString();
+}, 1000);
